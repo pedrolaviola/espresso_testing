e4b6a0ef730fab218e598b9070b3ddd58ac4d851
pedrolaviola <pedrolaviola@timcoo.com.br>
2018-10-02 16:48:36 -0300

    project removed

--- EspressoTesting/app/src/main/java/com/laviola/espressotesting/model/User.kt
+++
@@ -1,6 +0,0 @@
-package com.laviola.espressotesting.model
-
-import com.google.gson.annotations.SerializedName
-
-class User(@SerializedName("name")val name: String,
-           @SerializedName("job")val job : String)
